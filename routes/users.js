35c6d66f01f8d0fdbe122a6171bedb3f58db204d
hugo dandois <hugo.dandois@student.vinci.be>
2024-10-10 16:46:43 +0200

    update manageBadUser

--- routes/users.js
+++ routes/users.js
@@ -104,7 +104,7 @@ function matchingPWDlogin(req, userFound, res) {
 }
 
 function manageBadUser(req, res) {
-    console.log("bad user");
+    console.log("robignoule");
     req.session.errors = "Utilisateur inconnu";
     res.redirect('/users');
 }
