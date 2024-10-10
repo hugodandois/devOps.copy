d825784f2ce203d61fe83986258f476421a4d618
hugo dandois <hugo.dandois@student.vinci.be>
2024-10-10 16:10:51 +0200

    refactor

--- routes/users.js
+++ routes/users.js
@@ -28,29 +28,11 @@ router.post('/login', (req, res, next) => {
             res.redirect('/users');
         }
         else {
-            if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
-                console.log("password correct");
-                req.session.login = req.body.userLogin;
-                req.session.connected = true;
-                if (userFound.admin) {
-                    req.session.admin = true;
-                    res.redirect('/admin');
-                } else {
-                    req.session.admin = false;
-                    res.redirect('/members');
-                }
-            }
-            else {
-                console.log("bad password");
-                req.session.errors = "Mot de passe incorrect";
-                res.redirect('/users');
-            }
+            loginUserFoundActive(req, userFound, res);
         }
     }
     else {
-        console.log("bad user");
-        req.session.errors = "Utilisateur inconnu";
-        res.redirect('/users');
+        manageBadUser(req, res);
     }
 });
 
@@ -96,3 +78,29 @@ router.post('/add', (req, res, next) => {
 });
 
 module.exports = router;
+
+function loginUserFoundActive(req, userFound, res) {
+    if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
+        console.log("password correct");
+        req.session.login = req.body.userLogin;
+        req.session.connected = true;
+        if (userFound.admin) {
+            req.session.admin = true;
+            res.redirect('/admin');
+        } else {
+            req.session.admin = false;
+            res.redirect('/members');
+        }
+    }
+    else {
+        console.log("bad password");
+        req.session.errors = "Mot de passe incorrect";
+        res.redirect('/users');
+    }
+}
+
+function manageBadUser(req, res) {
+    console.log("bad user");
+    req.session.errors = "Utilisateur inconnu";
+    res.redirect('/users');
+}
